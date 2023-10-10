08bd5bc22f7b5bdc27275da5bb2ac9eb1695ca7b
Arathi <pythontrainer.tecswan@gmail.com>
2023-10-10 15:31:17 +0530

    connection

--- app1/models.py
+++ app1/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class guest_tbl(models.Model):
+class accounts(models.Model):
     userid = models.CharField(max_length=200)
+    status=models.CharField(max_length=200)
     class Meta:
-        db_table = 'guest_tbl' 
+        db_table = 'accounts' 
     
 class artist_tbl(models.Model):
     uname = models.CharField(max_length=200)
-    pasword = models.CharField(max_length=100)
     fname = models.CharField(max_length=200)
     lname = models.CharField(max_length=200)
     gender = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.BigIntegerField()
     ads = models.CharField(max_length=400)
+    photo=models.CharField(max_length=400)
     district = models.CharField(max_length=100)
     class Meta:
         db_table = 'artist_tbl'
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render ,redirect
-from app1.models import guest_tbl, artist_tbl
+from app1.models import accounts, artist_tbl
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.core.files.storage import FileSystemStorage
@@ -32,6 +32,7 @@ def addguest(request):
 def createartist1(request):
     p = artist_tbl()
     p1 = User()
+    p3=accounts()
     p.uname = request.POST.get('uname')
     p.fname = request.POST.get('fname')
     p.lname = request.POST.get('lname')
@@ -40,12 +41,31 @@ def createartist1(request):
     p.phone = request.POST.get('phone')
     p.district = request.POST.get('district')
     p.ads = request.POST.get('ads')
-    p.save()
+   
     p1.username = request.POST.get('uname')
     p1.first_name = request.POST.get('fname')
     p1.last_name = request.POST.get('lname')
     p1.email = request.POST.get('email')
     password = request.POST.get('password')
     p1.set_password(password)
+
+    p3.userid=request.POST.get('uname')
+    p3.status="artist" #"guest" in guest form
     p1.save()
+    p.save()
+    p3.save()
     return render(request, 'login_artist.html')
+
+def createguest1(request):
+   
+    p1 = User()
+    p3=accounts()
+    
+    p3.userid=request.POST.get('uname')
+    p3.status="guest"
+
+    p1.username = request.POST.get('uname')
+    p1.set_password("guest")
+    p3.save()
+    p1.save()
+    return render(request, "guest_login.html")
